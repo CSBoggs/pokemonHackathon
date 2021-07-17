f096297ea77f26b7a14389f918fb174058f1aaa0
CSBoggs <cboggs.dev@gmail.com>
2021-07-17 10:25:05 -0600

    implemented homepage and initialized pokemon arrays

---
+++ app.js
@@ -0,0 +1,18 @@
+var health = 100;
+var maxHealth = 100;
+
+
+var Pokemon {
+    construtor(name, sprite, hp, moves){
+        this.name = name;
+        this.sprite = sprite;
+        this.hp = hp;
+        this.fullhp = hp;
+        this.moves = moves;
+    }
+}
+
+var pkmList = [
+    'Squirtle', '\Projects\Pokemon\images\Squirtle.jpg'
+]
+}
