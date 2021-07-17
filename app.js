95afbd2028815e7ce2d479b9ab3892156afdcf32
CSBoggs <cboggs.dev@gmail.com>
2021-07-17 11:33:42 -0600

    added homepage css and battle page functions

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
-var health = 100;
-var maxHealth = 100;
+var Currenthealth = 100;
+var usermaxHealth = 100;
 
 
-var Pokemon {
-    construtor(name, sprite, hp, moves){
-        this.name = name;
-        this.sprite = sprite;
-        this.hp = hp;
-        this.fullhp = hp;
-        this.moves = moves;
-    }
+var pokemonList = document.querySelectorAll('pokemon-profile');
+// console.log(pokemonList);
+//     pokemonList.addEventListener("click", function playerSelect(){
+//         var characterSelect = pokemonList.Pokemon;
+//     }
+function characterSelect(catchEmAll){
+    var catchRow = document.createElement('div');
+    catchRow.classList.add('grid');
+    
+    var pokemonImg = document.createElement('img');
+    pokemonImg.setAttribute('src', player.imgLink);
+    catchRow.append(pokemonImg);
+
+    var name = document.createElement('h3');
+    name.innerText = player.name;
+    catchRow.append(name);
+
+    var attack1 = document.createElement('button')
+    attack1.innerHTML=attack.type;
+    catchRow.append(attack1);
+
+    var attack2 = document.createElement('button')
+    attack1.innerHTML=attack.type;
+    catchRow.append(attack2);
+
+    var attack3 = document.createElement('button')
+    attack1.innerHTML=attack.type;
+    catchRow.append(attack3);
 }
+console.log(characterSelect);
+    
+    
+
+
+// var Pokemon {
+//     construtor(name, sprite, hp, moves){
+//         this.name = name;
+//         this.sprite = sprite;
+//         this.hp = hp;
+//         this.fullhp = hp;
+//         this.moves = moves;
+//     }
+// }
 
 var pkmList = [
-    'Squirtle', '\Projects\Pokemon\images\Squirtle.jpg'
+    ['Squirtle', '\Projects\Pokemon\images\Squirtle.png',314]
+    ['Charmander', '\Projects\Pokemon\images\Charmander.png',309]
+    ['Bulbasaur', '\Projects\Pokemon\images\Bulbasaur.png',318]
 ]
+
+var typeMatch = {
+    'Squirtle': ['water', 'eletric']
+    
 }
