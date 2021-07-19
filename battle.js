2dad49776eb3bd5375538b9d35ca043cedb5bf1c
CSBoggs <cboggs.dev@gmail.com>
2021-07-19 14:53:32 -0600

    attemped to create functions to draw user pokemon selection on battle page

--- battle.js
+++ battle.js
@@ -2,19 +2,19 @@
 var pkmList = [
     {
         pokemon: 'squirtle',
-        image: '/Projects/Pokemon/images/Squirtle.png',
+        image: '/Projects/Pokemon/images/squirtle.png',
         hp: 100,
         attack: 50
     },
     {
         pokemon: 'charmander',
-        image: '/Projects/Pokemon/images/Charmander.png',
+        image: '/Projects/Pokemon/images/charmander.png',
         hp: 100,
         attack: 50
     },
     {
         pokemon: 'bulbasaur',
-        image: '/Projects/Pokemon/images/Bulbasaur.png',
+        image: '/Projects/Pokemon/images/bulbasaur.png',
         hp: 100,
         attack: 50
     }
@@ -41,61 +41,59 @@ var userAttacks = document.getElementById("userAttacks");
 var cpuAttacks = document.getElementById("cpuAttacks");
 var Currenthealth = 100;
 var usermaxHealth = 100;
-var chosenPokemon = [];
 
 // function update(){
 //     Cookies.set("battle", chosenPokemon, {expires: 10});
 // }
 
-function PokemonSelect(element){
-    var parent = element.parentElement;
-    var imgSrc = parent.querySelector('img').getAttribute('src');
-    var Pname = parent.querySelector('h3').innerText;
-    var PokemonInfo = {
-        imgLink : imgSrc,
-        name : Pname,
-    }
-    chosenPokemon.push(info);
-    update();
-}
-
-// Clear html element function to reset gamestate
-
-function clearElement(myElement) {
-    while (myElement.firstChild) {
-        myElement.removeChild(myElement.lastChild);
-    }
-}
+// function PokemonSelect(element){
+//     var parent = element.parentElement;
+//     var imgSrc = parent.querySelector('img').getAttribute('src');
+//     var Pname = parent.querySelector('h3').innerText;
+//     var PokemonInfo = {
+//         imgLink : imgSrc,
+//         name : Pname,
+//     }
+//     chosenPokemon.push(info);
+//     update();
+// }
 
 // Battle sequence logic
 
-pokemonBattle();
-
-function pokemonBattle() {
-    clearElement(userPokemon);
-    clearElement(cpuPokemon);
-}
-
 function makePokemonSprite(pokemonSelected) {
     var pokemonSprite = document.createElement("div");
-    pokemonSprite.classList.add(userPokemonSprite);
+    pokemonSprite.classList.add("userPokemonSprite");
     pokemonSprite.id = pokemonSelected;
 
     var pokemonName = document.createElement('h2');
     pokemonName.innerText = pokemonSelected;
     pokemonSprite.appendChild(pokemonName);
 
-var userPokemon = selectedPokemon(pokemonSelected);
-var userPokemonSprite = makePokemonSprite(pokemonSelected);
-battleViewport.id = userPokemon;
-userPokemonContainer.append(userPokemon);
+    var pokemonImage = document.createElement("img");
+    pokemonImage.src = '/Projects/Pokemon/images/squirtle.png';
+    pokemonSprite.appendChild(pokemonImage);
 
-var cpu = selectedPokemon(pokemonSelected);
-cpuPkm = userPokemon;
-cpuPokemonContainer.appendChild(userPokemon);
+    var pokemonHealth = document.createElement('p');
+    pokemonHealth.innerText = pokemonSelected.health;
 
 
-// var value = Cookies.get("name");
-// console.log(value);
-// var name = JSON.parse(value);
+    var pokemonAttack = document.createElement('p');
+    pokemonAttack.innerText = pokemonSelected.attack;
 
+    return pokemonSprite;
+}
+
+pokemonBattle();
+
+function pokemonBattle() {
+
+    var userPokemon = selectedPokemon(pokemonSelected);
+    var userPokemonSprite = makePokemonSprite(userPokemon);
+    battleViewport.id = userPokemon;
+    userPokemonSprite.append(userPokemon);
+}
+
+//     var cpuPokemon = selectedPokemon(pokemonSelected);
+//     cpuPkm = userPokemon;
+//     cpuPokemonSprite.append(userPokemon);
+// }
